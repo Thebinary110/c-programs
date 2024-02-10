1c73deaa334de78011c7f6340a316f94a63b77c4
Thebinary110 <152782737+Thebinary110@users.noreply.github.com>
2024-02-10 16:51:18 +0530

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -3,7 +3,13 @@
 
 void main (){
 
-printf("hello to my readers");
+printf("hello to my readers\n");
+printf("all the basic materials questions related to programming fundamentals will be uploaded\n");
+printf("the languages will be used will be c,c++,c#,html,css,js,react & redux , mongodb , sql , php etc");
+printf("the topics will be covered will be general questions and properties of languages basics and dsa problems");
+printf("and in the field of web development we will be using mern stack with html css javascript");
+printf("the projects will be posted on interval basis for educational purpose");
+printf("thank you");
 
 return 0;
 
